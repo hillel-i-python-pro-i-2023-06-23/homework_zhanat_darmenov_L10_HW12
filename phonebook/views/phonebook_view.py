e7fddba1129ad7bf1d4ea598e1e4e675338328d8
Zhanat Darmenov <Zhandarm2000@gmail.com>
2023-10-08 21:57:57 +0300

    Show all and return buttons

--- phonebook/views/phonebook_view.py
+++ phonebook/views/phonebook_view.py
@@ -1,22 +1,22 @@
 # phonebook/views/phonebook_view.py
 
 from django.shortcuts import render  # , redirect
+from phonebook.services.contact_management import generate_contacts, save_contacts, get_all_contacts
 
 # from django.http import HttpResponse
-from phonebook.services.contact_management import generate_contacts, save_contacts  # , get_all_contacts
-
 # from phonebook.models import Contact
 
 
 def home_page(request):
     if request.method == "POST":
-        amount = int(request.POST.get("amount"))
-        generated_contacts = list(generate_contacts(amount))
-        save_contacts(generated_contacts)
+        if request.POST.get("action") == "Generate":
+            amount = int(request.POST.get("amount"))
+            generated_contacts = list(generate_contacts(amount))
+            save_contacts(generated_contacts)
+            # Show only newly generated Contacts:
+            return render(request, "contact_list.html", {"contacts": generated_contacts})
 
-        # Show only newly generated Contacts:
-        return render(request, "contact_list.html", {"contacts": generated_contacts})
-
-        # Add "Show All Contacts" Button:
+        if request.POST.get("action") == "Show All":
+            return render(request, "contact_list.html", {"contacts": get_all_contacts()})
 
     return render(request, "home_page.html")
